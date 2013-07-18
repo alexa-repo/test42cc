0fa6512b830ac68ea126ac9aace4a67095b34422
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-18 18:06:14 +0300

    ticket:1 added first page with styles

--- test42cc/src/models.py
+++ test42cc/src/models.py
@@ -9,7 +9,7 @@ class Person(models.Model):
     email = models.EmailField(max_length=75)
     skype = models.CharField(max_length=40)
     jabber = models.CharField(max_length=75)
-    contacts = models.TextField()
+    other_contacts = models.TextField()
 
     def _get_full_name(self):
         return '%s %s' % (self.first_name, self.last_name)
--- test42cc/src/views.py
+++ test42cc/src/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from models import Person
+from django.shortcuts import render
+
+
+def index(request):
+    """
+    View for main page: render page with personal data
+    :param request:
+    :return:
+    """
+    try:
+        contact = Person.objects.get(pk=1)
+    except Person.DoesNotExist:
+        contact = None
+    return render(request, 'index.html', {'contact': contact})
