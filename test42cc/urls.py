d00f4977ad3510fbe9e979d58fe09c05eb5862f7
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-28 02:14:44 +0300

    ticket:6 makefile

--- test42cc/urls.py
+++ test42cc/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
                        url(r'^logout/$', 'django.contrib.auth.views.logout_then_login', name='logout'),
                        # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
-) #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
 
