247f22be578cc6e3f694a8639b70a56aa72827b9
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-27 21:21:11 +0300

    ticket:6 added widget for birthdate

--- test42cc/src/forms.py
+++ test42cc/src/forms.py
@@ -1,31 +1,14 @@
 from django.forms import ModelForm, Textarea
-from django.utils.safestring import mark_safe
 from models import Person
-from django.forms.widgets import ClearableFileInput, Input
-
-
-class CustomClearableFileInput(ClearableFileInput):
-    def render(self, name, value, attrs=None):
-        substitutions = {
-            'initial_text': self.initial_text,
-            'input_text': self.input_text,
-            'clear_template': '',
-            'clear_checkbox_label': '',
-        }
-        template = '%(input)s'
-        substitutions['input'] = Input.render(self, name, value, attrs)
-
-        return mark_safe(template % substitutions)
+from widget import *
 
 
 class PersonForm(ModelForm):
     class Meta:
         model = Person
-        widgets = {
-            'bio': Textarea({'cols': 40, 'rows': 10}),
-            'other_contacts': Textarea({'cols': 40, 'rows': 10}),
-            'image_photo': CustomClearableFileInput(),
-        }
+        widgets = dict(bio=Textarea({'cols': 40, 'rows': 10}), other_contacts=Textarea({'cols': 40, 'rows': 10}),
+                       image_photo=CustomClearableFileInput(),
+                       birth_date=DatePickerWidget(params="dateFormat: 'yy-mm-dd', changeYear: true"))
 
     def __init__(self, *args, **kwargs):
         super(PersonForm, self).__init__(*args, **kwargs)
---
+++ test42cc/src/widget.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.forms.widgets import ClearableFileInput, Input
+from django.utils.safestring import mark_safe
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    def render(self, name, value, attrs=None):
+        substitutions = {
+            'initial_text': self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': '',
+        }
+        template = '%(input)s'
+        substitutions['input'] = Input.render(self, name, value, attrs)
+
+        return mark_safe(template % substitutions)
+
+
+class DatePickerWidget(forms.DateInput):
+    class Media:
+        css = {
+            'all': ('http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css',)
+        }
+        js = (
+            'http://code.jquery.com/jquery-1.9.1.js',
+            'http://code.jquery.com/ui/1.10.3/jquery-ui.js',
+        )
+
+    def __init__(self, params='', attrs=None):
+        self.params = params
+        super(DatePickerWidget, self).__init__(attrs=attrs)
+
+    def render(self, name, value, attrs=None):
+        rendered = super(DatePickerWidget, self).render(name, value, attrs=attrs)
+        return rendered + mark_safe(u'''<script type="text/javascript">
+            $('#id_%s').datepicker({%s});
+            </script>''' % (name, self.params,))
