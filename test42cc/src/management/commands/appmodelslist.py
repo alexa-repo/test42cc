616a7b30ad27a61420bcac0e3dd3e61e11f56df3
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-28 18:29:38 +0300

    ticket:9 added command

---
+++ test42cc/src/management/commands/appmodelslist.py
@@ -0,0 +1,27 @@
+from optparse import make_option
+
+from django.core.management.base import AppCommand
+
+
+class Command(AppCommand):
+    option_list = AppCommand.option_list + (make_option('--err-stderr',
+                                                        action='store_true',
+                                                        dest='err',
+                                                        default=False,
+                                                        help='duplicate output to stderr'),)
+
+    requires_model_validation = True
+    help = 'Prints model names for given application and objects count'
+    args = '[appname ...]'
+
+    def handle_app(self, app, **options):
+        from django.db.models import get_models
+
+        for model in get_models(app):
+            err = options.get('err')
+            val = model.__name__ + " - %s objects" % model._default_manager.count()
+            if err:
+                self.stderr.write('error:%s' % val)
+            else:
+                self.stdout.write(val)
+
