3c81db4ef9fbdbc99eb991b99ec8138bded1ae4d
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-30 00:17:46 +0300

    ticket:5 change js

--- test42cc/static/js/form_edit.js
+++ test42cc/static/js/form_edit.js
@@ -1,26 +1,35 @@
-$(function(){
-    $('#form_id').submit(function(){
+$(function () {
+    $('#form_id').submit(function () {
         $(this).ajaxSubmit({
-            beforeSend: function(){
+            beforeSend: function () {
+                $('#progress').html("Progress saving data...");
                 $('#progress').show();
-                $('input:submit').attr("disabled","disabled");
+                $('input:submit').attr("disabled", "disabled");
+                $('#form_id input:text').attr("disabled", "disabled");
+                $('#form_id textarea').attr("disabled", "disabled");
+                $('#form_id input:file').attr("disabled", "disabled");
             },
-            success : function(responce) {
-                $("#progress").hide();
+            success: function (responce) {
                 $('input:submit').removeAttr("disabled");
+                $('#form_id input:text').removeAttr("disabled");
+                $('#form_id textarea').removeAttr("disabled");
+                $('#form_id input:file').removeAttr("disabled");
                 data = $.parseJSON(responce);
-                if (data['status'] == 1){
+                if (data['status'] == 1) {
+                    $("#progress").hide();
                     err = data['errors'];
-                    $.each(err, function(name, error) {
+                    $.each(err, function (name, error) {
                         $("#errors_list").append(name + ": " + error);
                     });
-                                       }
-                else{
-                    window.location.href = data['redirect'];
+                }
+                else {
+                    $('#progress').html("Form data have saved");
+                    $('#progress').css({color: "green"});
+                    //window.location.href = data['redirect'];
                 }
             }
         });
-    return false;
+        return false;
     });
 });
 
