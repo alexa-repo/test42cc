ee949c9d9c4d79e193101faa7b77c54c6aa78f53
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-29 21:56:29 +0300

    ticket:1 relocate templates

--- test42cc/settings.py
+++ test42cc/settings.py
@@ -135,7 +135,7 @@ TEMPLATE_DIRS = (
     # C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(PROJECT_ROOT, 'src/templates'),
 )
 
 INSTALLED_APPS = (
