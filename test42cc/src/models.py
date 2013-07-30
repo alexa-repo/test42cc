ce2c1983c4f4fa30fa64e3aa45cc6344d25f11a8
alexa-sandra <alexa.sandra.mail@gmail.com>
2013-07-30 22:20:17 +0300

    ticket:10 change field and signals

--- test42cc/src/models.py
+++ test42cc/src/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
 
 
 class Person(models.Model):
@@ -12,7 +13,9 @@ class Person(models.Model):
     skype = models.CharField(max_length=40, verbose_name="Skype name")
     jabber = models.CharField(max_length=75, verbose_name="Jabber ID")
     other_contacts = models.TextField(verbose_name="Other contacts")
-    image_photo = models.ImageField(verbose_name="Photo", upload_to='images/uploads', null=True, blank=True)
+    image_photo = models.ImageField(verbose_name="Photo",
+                                    upload_to='images/uploads', null=True,
+                                    blank=True)
 
     def _get_full_name(self):
         return '%s %s' % (self.first_name, self.last_name)
@@ -27,7 +30,8 @@ class HttpStoredQuery(models.Model):
     path = models.CharField(max_length=300)
     method = models.CharField(max_length=20)
     user = models.ForeignKey(User, blank=True, null=True)
-    date_with_time = models.DateTimeField(auto_now_add=True, default=datetime.datetime.now())
+    date_with_time = models.DateTimeField(auto_now_add=True,
+                                          default=datetime.datetime.now())
     priority = models.IntegerField(default=1)
 
 
@@ -47,5 +51,40 @@ class ModelsActions(models.Model):
 
     action = models.IntegerField(choices=STATUS_CHOICES, default=CREATE_ACTION)
     model_name = models.CharField(max_length=75)
-    change_obj_id = models.IntegerField()
+    change_obj_id = models.PositiveIntegerField()
     date_with_time = models.DateTimeField(auto_now=True)
+
+
+def save_model_signal(sender, **kwargs):
+    """
+    Save data by post save signal. Ignore model ModelsActions
+    """
+    instance = kwargs['instance']
+    if sender == ModelsActions or type(instance.pk) != int:
+        return
+
+    mod = ModelsActions()
+    mod.model_name = sender.__name__
+    mod.change_obj_id = instance.pk
+    if not kwargs['created']:
+        mod.action = ModelsActions.UPDATE_ACTION
+    mod.save()
+
+
+def delete_model_signal(sender, **kwargs):
+    """
+    Save entry about delete some object from model. Ignore model ModelsActions
+    """
+    instance = kwargs['instance']
+    if sender == ModelsActions or type(instance.pk) != int:
+        return
+
+    mod = ModelsActions()
+    mod.model_name = sender.__name__
+    mod.change_obj_id = instance.pk
+    mod.action = ModelsActions.DELETE_ACTION
+    mod.save()
+
+
+post_save.connect(save_model_signal)
+post_delete.connect(delete_model_signal)
